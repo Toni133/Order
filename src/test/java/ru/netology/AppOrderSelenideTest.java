f8100bc72a87231c2b70613cd4d66cbc767e7739
Anton <toniq133@bk.ru>
2023-12-02 17:17:33 +0300

    first commit

---
+++ src/test/java/ru/netology/AppOrderSelenideTest.java
@@ -0,0 +1,28 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class AppOrderSelenideTest {
+    @BeforeEach
+    void openBrowser(){
+        open("http://localhost:9999/");
+    }
+    @Test
+    void shouldTestFormSuccess(){
+        SelenideElement name = $("[data-test-id=\"name\"]");
+        SelenideElement phone = $("[data-test-id=\"phone\"]");
+        name.$("[data-test-id=name] input").setValue("Василий Васильков");
+        phone.$("[data-test-id=phone] input").setValue("+79100000000");
+        SelenideElement checkbox = $("[data-test-id=\"agreement\"]");
+        SelenideElement button = $("[class=\"button__content\"]");
+        checkbox.click();
+        button.click();
+        $("[data-test-id=\"order-success\"]").shouldHave(Condition.text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
